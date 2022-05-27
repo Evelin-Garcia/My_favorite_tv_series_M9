7d42c0ce814a180262f4550fcd4ed111a0f1582a
Evelin Garcia <e.garcia.santander@gmail.com>
2022-05-27 18:10:26 -0400

    agregar tablas al formulario

--- app/models/tv_show.rb
+++ app/models/tv_show.rb
@@ -1,4 +1,8 @@
 class TvShow < ApplicationRecord
+  belongs_to :nationality
   has_many :episodes, dependent: :destroy
+  has_many :film_locations, inverse_of: :tv_show
+  accepts_nested_attributes_for :film_locations
+
   validates :name, presence: true
 end
